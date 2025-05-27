0324441a584f2999970038ef665fe70bfc489beb
aleksandra <matealeksandra@gmail.com>
2025-05-27 14:09:27 +0300

    Updates

---
+++ precache-manifest.55681a0fe683132aad1cee7d18d8ba2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239e965adf8685512f0debc39860f8c9",
+    "url": "/hws2-master/index.html"
+  },
+  {
+    "revision": "ddefbc6c8f965e70e543",
+    "url": "/hws2-master/static/css/main.b7ef7715.chunk.css"
+  },
+  {
+    "revision": "e7601ede402b0c8ee432",
+    "url": "/hws2-master/static/js/2.e1f72586.chunk.js"
+  },
+  {
+    "revision": "ddefbc6c8f965e70e543",
+    "url": "/hws2-master/static/js/main.33a42b03.chunk.js"
+  },
+  {
+    "revision": "9b29a5f2bdc42e86cc7d",
+    "url": "/hws2-master/static/js/runtime~main.bae38d49.js"
+  },
+  {
+    "revision": "0f8da6725623da0146311cab8cdf2954",
+    "url": "/hws2-master/static/media/avatar.0f8da672.png"
+  }
+]);
