d011ad9180a7b88ab38bba15983655e3422d6ad7
aleksandra <matealeksandra@gmail.com>
2025-06-27 09:07:27 +0200

    homework 06

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -18,8 +18,8 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
-        // делают студенты
-
+        const restoredValue = restoreState<string>('hw6-editable-span-value', '')
+        setValue(restoredValue)
     }
 
     return (
