84680e3a685b5b3a246c9cd1f2e33453eab5bbf5
aleksandra <matealeksandra@gmail.com>
2025-07-30 11:46:40 +0200

    bug fix

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,51 +1,53 @@
-// HW11.tsx
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import {restoreState} from '../hw06/localStorage/localStorage'
+import { restoreState } from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 function HW11() {
-    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
+    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 10))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    // Типизация слайдера из MUI: value может быть числом или массивом чисел (для диапазона)
-    const change = (event: Event, value: number | number[]) => {
-        if (Array.isArray(value)) {
-            setValue1(value[0])
-            setValue2(value[1])
-        } else {
-            setValue1(value)
-        }
+    const handleSingleChange = (event: Event, newValue: number | number[]) => {
+        const val = newValue as number
+        setValue1(val)
+        if (val > value2) setValue2(val)
+    }
+
+    const handleDoubleChange = (event: Event, newValue: number | number[]) => {
+        const [val1, val2] = newValue as number[]
+        setValue1(val1)
+        setValue2(val2)
     }
 
     return (
-        <div id={'hw11'}>
+        <div id="hw11" className={s2.container}>
             <div className={s2.hwTitle}>Homework #11</div>
-
             <div className={s2.hw}>
                 <div className={s.container}>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-single-slider'}
-                            value={value1}
-                            onChange={change}
-                            min={0}
-                            max={100}
-                        />
+
+                    <div id="hw11-single-slider" className={s.sliderWrapper}>
+                        <span id="hw11-value" className={s.valueLabel}>{value1}</span>
+                        <div className={s.sliderContainer}>
+                            <SuperRange
+                                value={value1}
+                                onChange={handleSingleChange}
+                            />
+                        </div>
+                        <span className={`${s.valueLabel} ${s.rightValue}`}></span>
                     </div>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-double-slider'}
-                            value={[value1, value2]}
-                            onChange={change}
-                            min={0}
-                            max={100}
-                        />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+
+                    <div id="hw11-double-slider" className={s.sliderWrapper}>
+                        <span id="hw11-value-1" className={s.valueLabel}>{value1}</span>
+                        <div className={s.sliderContainer}>
+                            <SuperRange
+                                value={[value1, value2]}
+                                onChange={handleDoubleChange}
+                            />
+                        </div>
+                        <span id="hw11-value-2" className={`${s.valueLabel} ${s.rightValue}`}>{value2}</span>
                     </div>
+
                 </div>
             </div>
         </div>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,29 +1,43 @@
-// common/c7-SuperRange/SuperRange.tsx
 import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import type { SliderProps } from '@mui/material/Slider'
+import Slider from '@mui/material/Slider'
+import { styled } from '@mui/material/styles'
+
+const StyledSlider = styled(Slider)(({ value }) => ({
+    width: '100%',
+    height: 4,
+    padding: '10px 0 !important',
+    margin: '0 !important',
+    color: Array.isArray(value) ? '#D9D9D9' : '#00CC22',
+
+    '& .MuiSlider-rail': {
+        width: '100% !important',
+        height: 4,
+        backgroundColor: '#D9D9D9 !important',
+        opacity: '1 !important'
+    },
+
+    '& .MuiSlider-track': {
+        backgroundColor: '#00CC22 !important',
+        border: 'none !important',
+        height: 4
+    },
+
+    '& .MuiSlider-thumb': {
+        width: '18px !important',
+        height: '18px !important',
+        backgroundColor: '#00CC22 !important',
+        border: '1px solid white !important',
+        boxShadow: '0 0 0 1px #00CC22 !important',
+
+        '&:hover, &.Mui-active': {
+            boxShadow: '0 0 0 8px rgba(0, 204, 34, 0.2) !important'
+        }
+    }
+}))
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{
-                color: '#00CC22',
-                height: 8,
-                '& .MuiSlider-track': {
-                    border: 'none',
-                },
-                '& .MuiSlider-thumb': {
-                    height: 24,
-                    width: 24,
-                    backgroundColor: '#fff',
-                    border: '2px solid currentColor',
-                    '&:focus, &:hover, &.Mui-active': {
-                        boxShadow: '0 0 0 8px rgba(63, 81, 181, 0.16)',
-                    },
-                },
-            }}
-            {...props}
-        />
-    )
+    return <StyledSlider {...props} />
 }
 
 export default SuperRange
