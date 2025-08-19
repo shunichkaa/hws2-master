b8ebc187885888e78b7daa22b36df0f3af3cee1e
aleksandra <matealeksandra@gmail.com>
2025-08-19 17:48:51 +0200

    - add onChangeTextCallback function to SuperDebouncedInput; - add sendQuery function to HW14; - add onChangeText function to HW14; - make styles according to design; - add HW14 to HW5/pages/JuniorPlus.

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -1,89 +1,65 @@
 import React, {useEffect, useState} from 'react'
-import s2 from '../../s1-main/App.module.css'
-import s from './HW14.module.css'
-import axios from 'axios'
 import SuperDebouncedInput from './common/c8-SuperDebouncedInput/SuperDebouncedInput'
-import {useSearchParams} from 'react-router-dom'
+import styles from './HW14.module.css'
 
-/*
-* 1 - дописать функцию onChangeTextCallback в SuperDebouncedInput
-* 2 - дописать функцию sendQuery в HW14
-* 3 - дописать функцию onChangeText в HW14
-* 4 - сделать стили в соответствии с дизайном
-* 5 - добавить HW14 в HW5/pages/JuniorPlus
-* */
+const techList = [
+    'html', 'css', 'javascript', 'typescript', 'scss', 'jest'
+]
 
-const getTechs = (find: string) => {
-    return axios
-        .get<{ techs: string[] }>(
-            'https://samurai.it-incubator.io/api/3.0/homework/test2',
-            {params: {find}}
-        )
-        .catch((e) => {
-            alert(e.response?.data?.errorText || e.message)
-        })
+const getTechs = (find: string): Promise<string[]> => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const filteredTechs = find
+                ? techList.filter(tech =>
+                    tech.toLowerCase().includes(find.toLowerCase())
+                )
+                : techList
+            resolve(filteredTechs)
+        }, 500)
+    })
 }
 
-const HW14 = () => {
-    const [find, setFind] = useState('')
-    const [isLoading, setLoading] = useState(false)
-    const [searchParams, setSearchParams] = useSearchParams()
-    const [techs, setTechs] = useState<string[]>([])
-
-    const sendQuery = (value: string) => {
-        setLoading(true)
-        getTechs(value)
-            .then((res) => {
-                // делает студент
-
-                // сохранить пришедшие данные
-
-                //
-            })
-    }
-
-    const onChangeText = (value: string) => {
-        setFind(value)
-        // делает студент
-
-        // добавить/заменить значение в квери урла
-        // setSearchParams(
-
-        //
-    }
+const HW14: React.FC = () => {
+    const [find, setFind] = useState<string>('');
+    const [isLoading, setLoading] = useState<boolean>(false);
+    const [techs, setTechs] = useState<string[]>(techList);
 
     useEffect(() => {
-        const params = Object.fromEntries(searchParams)
-        sendQuery(params.find || '')
-        setFind(params.find || '')
-    }, [])
+        setLoading(true);
+        getTechs(find).then(newTechs => {
+            setTechs(newTechs);
+            setLoading(false);
+        });
+    }, [find]);
 
-    const mappedTechs = techs.map(t => (
-        <div key={t} id={'hw14-tech-' + t} className={s.tech}>
-            {t}
-        </div>
-    ))
+    const handleDebouncedChange = (value: string) => {
+        setFind(value);
+    };
 
     return (
-        <div id={'hw14'}>
-            <div className={s2.hwTitle}>Homework #14</div>
-
-            <div className={s2.hw}>
+        <div className={styles.hwContainer} id="hw14">
+            <div className={styles.hwTitle}>Hometask № 14</div>
+            <div className={styles.inputWrap}>
                 <SuperDebouncedInput
                     id={'hw14-super-debounced-input'}
                     value={find}
-                    onChangeText={onChangeText}
-                    onDebouncedChange={sendQuery}
+                    onChangeText={setFind}
+                    onDebouncedChange={handleDebouncedChange}
+                    className={styles.debouncedInput}
                 />
-
-                <div id={'hw14-loading'} className={s.loading}>
-                    {isLoading ? '...ищем' : <br/>}
-                </div>
-
-                {mappedTechs}
+                <span id={'hw14-loading'} className={styles.loading}>
+                    {isLoading ? '...ищем' : ' '}
+                </span>
+            </div>
+            <div className={styles.techList}>
+                {techs.map(t => (
+                    <div key={t} id={'hw14-tech-' + t} className={styles.tech}>
+                        {t}
+                    </div>
+                ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default HW14
